ba783ac863a05a4a0d7b61eaabaf2c52100005af
wisdom08 <wisdom08@kakao.com>
2022-08-04 20:52:52 +0900

    feat: 토큰 리포지토리 추가
    커밋 누락으로 토큰 관련 커밋과 따로 커밋합니다.

---
+++ src/main/java/com/wisdom/blog/repository/refreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.wisdom.blog.repository;
+
+import com.wisdom.blog.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface refreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(String userId);
+}
